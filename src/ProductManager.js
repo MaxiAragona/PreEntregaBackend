1f102abc7a9c881bf6cb672bc1488ab11dfb5db4
maxiaragona <aragona_9@hotmail.com>
2024-03-21 00:15:05 -0300

    primera entrega backend

---
+++ src/ProductManager.js
@@ -0,0 +1,111 @@
+const fs = require("fs");
+
+class ProductManager {
+    constructor(path) {
+        this.path = path;
+        this.init();
+    }
+
+    async init() {
+        const products = await this.getProducts()
+        await this.setProducts(products);
+    }
+
+    async getProducts() {
+        try {
+            return JSON.parse(fs.readFileSync(this.path));
+        } catch {
+            return [];
+        }
+    }
+
+    async setProducts(products) {
+        fs.writeFileSync(this.path, JSON.stringify(products, null, 2));
+    }
+
+    async addProduct(product) {
+        const { title, description, price, imagenes, code, stock, status = true } = product
+        if (!title || !description || !price || !code || !stock) {
+            throw new Error("Todos los campos son obligatorios");
+        }
+
+        const products = await this.getProducts()
+
+        if (products.some(product => product.code === code)) {
+            throw new Error("El codigo debe ser unico");
+        }
+
+        const newProduct = {
+            id: (await this.getLastId())+1,
+            title,
+            description,
+            price,
+            imagenes,
+            code,
+            stock,
+            status
+        }
+        products.push(newProduct);
+        await this.setProducts(products);
+
+        return newProduct;
+    }
+
+    async updateProduct(id, data) {
+        const products = await this.getProducts()
+        const product = products.find(product => product.id === id);
+
+        if (!product) {
+            return;
+        }
+
+        product.title = data.title;
+        product.description = data.description;
+        product.price = data.price;
+        product.img = data.img;
+        product.code = data.code;
+        product.stock = data.stock;
+
+        await this.setProducts(products);
+    }
+
+    async getProductById(id) {
+        const products = await this.getProducts()
+        const product = products.find(product => product.id === id);
+
+        if (!product) {
+            console.error("No se ha encontrado el producto.");
+
+        } else {
+            console.log("Producto encontrado.", product);
+        }
+        return product;
+    }
+
+    async deleteProduct(id) {
+        const products = await this.getProducts();
+        const productIndex = products.findIndex(product => product.id === id);
+
+        if (productIndex < 0) {
+            console.error("No se ha encontrado el producto.");
+            return;
+        }
+
+        products.splice(productIndex, 1);
+       await this.setProducts(products);
+    }
+
+    async getLastId(){
+        const products = await this.getProducts();
+        let id = 0;
+
+        products.forEach(product => {
+            if(product.id > id ) {
+                id = product.id;
+            }
+        });
+        return id;
+    }
+}
+
+module.exports = ProductManager;
---
+++ src/app.js
@@ -0,0 +1,45 @@
+const ProductManager = require("./ProductManager")
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.use(express.json());
+
+// creamos la instancia del manager
+const manager = new ProductManager("./src/maxProducts.json");
+
+app.get('/products', async (req, res) => {
+    const products = await manager.getProducts();
+    let limit = parseInt(req.query.limit);
+
+    if(isNaN(limit)){
+        res.send(products);
+    } else {
+        res.send(products.slice(0, limit))
+    }
+})
+
+app.get("/products/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+    const product = await manager.getProductById(id);
+
+    if(product) {
+        res.send(product);
+    } else {
+        res.status(404).send("producto no encontrado");
+    }
+})
+
+app.post('/products', async (req, res) => {
+    try {
+        const product = await manager.addProduct(req.body);
+        res.send(product);
+    } catch (error) {
+        res.status(400).send(error.message);   
+    }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
